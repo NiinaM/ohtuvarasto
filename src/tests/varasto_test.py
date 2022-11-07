735310be3e331c54faf9a68478ed98a2dd992685
Niina-Maria Takalo <niina-maria.takalo@helsinki.fi>
2022-11-07 19:59:56 +0200

    testit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,13 +10,30 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_uudella_varastolla_ei_negatiivista_tilavuutta(self):    
+        varasto2 = Varasto(-1)
+
+        self.assertAlmostEqual(varasto2.tilavuus, 0.0)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    def test_alkusaldo_ei_negatiivinen(self):
+        varasto3 = Varasto(10, -1)
+
+        self.assertAlmostEqual(varasto3.saldo, 0.0)
+
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisaa_varastoon_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +55,25 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ota_varastosta_negatiivinen_maara(self):
+        self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_laita_liian_paljon_tavaraa(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_otetaan_pois_tavaraa_enemman_kuin_on(self):
+        self.varasto.ota_varastosta(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_palauttaako_str_oikein(self):
+        self.varasto.lisaa_varastoon(3)
+
+        self.assertEqual(self.varasto.__str__(), "saldo = 3, vielä tilaa 7")
+
+
